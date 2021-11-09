6b3742910f76d6510072786f4bb7ef26fc348cff
falcatamaster <81315959+falcatamaster@users.noreply.github.com>
2021-11-09 23:29:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,63 +1,85 @@
 'use strict';
 
 {
-    function setWord() {
-        word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
-        target.textContent = word;
-        loc = 0 ;
-    }
-    
-    let words;
-    let word;
-    let loc;
-    let startTime;
-    let isPlaying = false;
-    const target = document.getElementById('target');
+    const question = document.getElementById('question');
+    const choices = document.getElementById('choices');
+    const btn = document.getElementById('btn');
     const result = document.getElementById('result');
+    const scoreLabel = document.querySelector('#result > p');
 
-    document.addEventListener('click',() => {
-        if (isPlaying) {
+    const quizSet = shuffle([
+        {q: '世界で一番大きな島は？', c: ['グリーンランド', 'シチリア島', '佐渡島']},
+        {q: 'What is the thing you like most?', c: ['Me', 'Manchester City', 'Noel Gallagher']},
+        {q: 'What is something you hate?', c: ['Manchester United', 'Manchester United', 'Manchester United']},
+        {q: 'SpongeBob', c: ['legend', 'tall dark handsome', 'good for the soul']},
+    ]);
+    let currentNum = 0;
+    let isAnswered;
+    let score = 0;
+    
+    function shuffle(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[j], arr[i]] = [arr[i], arr[j]];
+        }
+        return arr;
+    }
+
+    function checkAnswer(li) {
+        if (isAnswered) {;
             return;
         }
+        isAnswered = true;
 
-        words = [
-            'wanderwall',
-            'whatever',
-            'live forever',
-            'morning glory',
-            'some might say',
-            'the importance of being idle',
-            'acquiesce',
-            'supersonic',
-            'songbird'
-        ];
-        result.textContent = ``;
-        isPlaying = true;
-        startTime = Date.now();
-        setWord();
-    })
+        if (li.textContent === quizSet[currentNum].c[0]) {
+            li.classList.add('correct');
+            score++;
+        } else {
+            li.classList.add('wrong');
+        }
 
-    document.addEventListener('keydown', e => {
-        // if (e.key !== word[loc]) {
-        //     return;
-        // }
+        btn.classList.remove('disabled');
+    }
+    
+    function setQuiz() {
+        isAnswered = false;
 
-        loc++;
+        question.textContent = quizSet[currentNum].q;
 
-        // 1:_ed
-        // 2:__d
-        // 3:___
-        target.textContent = '_'.repeat(loc) + word.substring(loc);
+        while (choices.firstChild) {
+            choices.removeChild(choices.firstChild);
+        }
+    
+        const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+        shuffledChoices.forEach(choice => {
+            const li = document.createElement('li');
+            li.textContent = choice;
+            li.addEventListener('click', () => {
+                checkAnswer(li);
+            });
+            choices.appendChild(li);
+        });
 
-        if (loc === word.length) {
-            if (words.length === 3) {
-                const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
-                result.textContent = `Finished! ${elapsedTime} seconds! Click to start`;
-                isPlaying = false;
-                return;
-            }
+        if (currentNum === quizSet.length - 1) {
+            btn.textContent = 'Show Score';
+        }
+    }
 
-            setWord();
+    setQuiz();
+
+    btn.addEventListener('click', () => {
+        if (btn.classList.contains('disabled')) {
+            return;
+        }
+        btn.classList.add('disabled');
+
+        if (currentNum === quizSet.length - 1) {
+            // console.log(`Score: ${score} / ${quizSet.length}`);
+            scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+            result.classList.remove('hidden');
+        } else {
+            currentNum++;
+            setQuiz();
         }
     });
 }
